7f9a4cf3871df9798196a7ff18f8ba0b5ef864ec
Camilo Martinez <imagina3ddesign@gmail.com>
2025-10-31 09:02:52 -0500

    camvios en es sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-bim-alcabama-v1';
+const CACHE_NAME = 'portal-bim-alcabama-v2'; // Incrementamos la versión
 // Lista de archivos que queremos guardar en caché para que la app funcione offline.
 const urlsToCache = [
   'home.html', // Ruta relativa a la ubicación del Service Worker
