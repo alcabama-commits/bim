2254b083026c0358722985d537c6bdc31d028ac2
Camilo Martinez <imagina3ddesign@gmail.com>
2025-11-04 08:32:38 -0500

    subida cambio

--- libs/three.module.js
+++
@@ -1,2 +0,0 @@
-// Contenido completo de la librería three.module.js r152
-// Este es un archivo muy grande. Al aplicar este cambio, se llenará con todo el código necesario.
--- libs/web-ifc-viewer.es.js
+++
@@ -1,2 +0,0 @@
-// Contenido completo de la librería web-ifc-viewer.es.js v1.0.233
-// Este es un archivo muy grande. Al aplicar este cambio, se llenará con todo el código necesario.
