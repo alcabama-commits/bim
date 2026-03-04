c62e66f2f75b9911032dadbeaa7045f281a6bada
Camilo Martinez <imagina3ddesign@gmail.com>
2026-03-04 13:32:54 -0500

    conectar con scrpit

--- ENTREGA_PROPIETARIOS_MAG/src/config.ts
+++ ENTREGA_PROPIETARIOS_MAG/src/config.ts
@@ -1,4 +1,4 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwGTZM6MWrR-9rqvEhQjS02hg53sPkFt3j7eb6j-hB6klPfBQXZ96fxrMBRypnVkt4S/exec'; // TODO: Replace with your deployed Web App URL
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz7FNMd35YSgKtwu6XP0TUmiQhWhAMlA11Z2IR3gN5SEV3pUUqconfIujA1HcU8DvBQ/exec'; // TODO: Replace with your deployed Web App URL
 
 export const API_CONFIG = {
   scriptUrl: SCRIPT_URL,
