5ad6015136c419e6299759f7aab2eb4a4ddb7931
Camilo Martinez <imagina3ddesign@gmail.com>
2026-03-05 14:16:26 -0500

    borrar vistas

--- VSR_IFC/src/config.ts
+++ VSR_IFC/src/config.ts
@@ -2,4 +2,4 @@
 // Configuration for VSR_IFC
 
 // Replace this with your deployed Google Apps Script Web App URL
-export const VIEWPOINTS_API_URL = 'https://script.google.com/macros/s/AKfycbyP-sR4wOd59qL0RIkTou8TcNO5hjK7DjM500Yh7Izp1o20VGNCozN5c7jTMhfDDCA/exec'; 
+export const VIEWPOINTS_API_URL = 'https://script.google.com/macros/s/AKfycbyDorjQlLY4AI8DMjUtwhn1mBv_OSfKN0PohAeKvegzw5no7cRR3mXQaroVVsN0h7c/exec'; 
