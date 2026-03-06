e9d0861961669beef10308b00a52014fff4a6d2f
Camilo Martinez <imagina3ddesign@gmail.com>
2026-03-06 11:55:46 -0500

    despliegue nuevo de Script

--- VSR_IFC/src/config.ts
+++ VSR_IFC/src/config.ts
@@ -2,5 +2,5 @@
 // Configuration for VSR_IFC
 
 // Replace this with your deployed Google Apps Script Web App URL
-export const VIEWPOINTS_API_URL = 'https://script.google.com/macros/s/AKfycbxycg4j2j7Mr-IkIanYND7hVubmgxf3gZDNBT9G2oKtDhyBfea_m9oVHFp4p6KcNbU/exec'; 
+export const VIEWPOINTS_API_URL = 'https://script.google.com/macros/s/AKfycbw7m7Fs_Ly7rpC8RKsVjs-ed6ncPWAghffUilhmTdy_xhI0fbkW1Q1UDFtKK1Ndk9g/exec'; 
 despliegue
