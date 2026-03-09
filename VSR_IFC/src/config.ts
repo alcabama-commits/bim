7f781a94a4cce0dbfe296ecb62c29c28c33ff33c
Camilo Martinez <imagina3ddesign@gmail.com>
2026-03-09 13:43:18 -0500

    cambio en la vista

--- VSR_IFC/src/config.ts
+++ VSR_IFC/src/config.ts
@@ -2,4 +2,4 @@
 // Configuration for VSR_IFC
 
 // Replace this with your deployed Google Apps Script Web App URL
-export const VIEWPOINTS_API_URL = 'https://script.google.com/macros/s/AKfycbzQJUWJMKtaiYo_TzDsUnNsdyCCDfQI8WSK6Ljo9m_OQ9yowvlhHSlbBkkXijR2mKk/exec';
+export const VIEWPOINTS_API_URL = 'https://script.google.com/macros/s/AKfycbwNJsV_5iSZK18kE6Ip3BMG_Nk4T9CdNqbX72h6Vq2rjAXks7wn4N18QVLcVxg96XQ/exec';
