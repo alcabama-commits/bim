1530ce724afd8940b17787f75592685322080e74
Camilo Martinez <imagina3ddesign@gmail.com>
2026-03-16 13:21:26 -0500

    Fix GitHub Pages fetch: unregister service worker

--- ENTREGA_PROPIETARIOS_MAG/src/main.tsx
+++ ENTREGA_PROPIETARIOS_MAG/src/main.tsx
@@ -3,6 +3,14 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  }).catch(() => {});
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
