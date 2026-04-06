735f8b1eea2c3487b3dd0fb5353efeaa65473fa4
Camilo Martinez <imagina3ddesign@gmail.com>
2026-04-06 13:22:50 -0500

    STATUS

--- STATUS/src/App.tsx
+++ STATUS/src/App.tsx
@@ -407,6 +407,7 @@ export default function App() {
 
       await fetch(DRIVE_SCRIPT_WEBAPP_URL, {
         method: 'POST',
+        mode: 'no-cors',
         headers: { 'Content-Type': 'text/plain;charset=utf-8' },
         body: JSON.stringify(payload)
       });
