f444f70b00dc90fe24337302621983d1fb91cc42
Camilo Martinez <imagina3ddesign@gmail.com>
2026-04-27 08:55:28 -0500

    Nuevos proyectos en el home y caché

--- CANTIDADES/src/main.tsx
+++ CANTIDADES/src/main.tsx
@@ -4,8 +4,10 @@ import App from './App.tsx';
 import './index.css';
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: './' }).catch(() => {});
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
   });
 }
 
